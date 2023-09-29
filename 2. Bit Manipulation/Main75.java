e6a65aff54108d296663409424a82caf505d2a9b
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-09-29 22:36:32 +0530

    L-1342 Reduce N to 1.

---
+++ 2. Bit Manipulation/Main75.java
@@ -0,0 +1,47 @@
+
+//  https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
+//  https://practice.geeksforgeeks.org/problems/minimum-steps-to-minimize-n-as-per-given-condition0618/1
+
+
+import java.util.*;
+
+public class Main75 {
+
+    public static int solution(long n) {
+        
+        int step = 0;
+        while(n != 1){
+           if((n & 1) == 0) n /= 2;       // 4x + 0
+           else if(n == 3) n--;          
+           else if((n & 3) == 1) n--;     // 4x + 1
+           else if((n & 3) == 3) n++;     // 4x + 3
+
+           step ++;
+        }
+        return step;
+    }
+    
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+        System.out.println(solution(n));
+
+        scn.close();
+    }
+
+}
+
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  
+//  int step = 0;
+//         while(n != 1){
+//            if((n % 2) == 0) n /= 2;       // 4x + 0
+//            else if(n == 3) n--;          
+//            else if((n % 4) == 1) n--;     // 4x + 1
+//            else if((n % 4) == 3) n++;     // 4x + 3
+
+//            step ++;
+//         }
+//         return step;
