b21572897f4a87d5e7e05c940f6043bb2cef3634
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-01 22:26:53 +0530

    L-393 UTF-8 Encoding.

---
+++ 2. Bit Manipulation/Main84.java
@@ -0,0 +1,56 @@
+
+//  https://leetcode.com/problems/utf-8-validation/
+
+
+import java.util.*;
+
+public class Main84 {
+
+    public static boolean solution(int[] arr) {
+        
+        int remBytes = 0;
+
+        for(int val : arr){
+            if(remBytes == 0){
+                if((val >> 7) == 0b0){
+                    remBytes = 0;
+                }else if((val >> 5) == 0b110){
+                    remBytes = 1;
+                }else if((val >> 4) == 0b1110){
+                    remBytes = 2;
+                }else if((val >> 3) == 0b11110){
+                    remBytes = 3;
+                }
+                else{
+                    return false;
+                }
+            }else{
+                if((val >> 6) == 0b10){
+                    remBytes--;
+                }else if((val >> 6) != 0b10){
+                    return false;
+                }
+            }
+        }
+    
+        if(remBytes == 0){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+	public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+
+        scn.close();
+    }
+    
+    
+}
