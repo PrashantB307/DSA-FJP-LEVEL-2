b3563d8a95f92099a131ba3867d3d83efdcb7018
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-08 11:56:47 +0530

    Combinations - 2.

---
+++ 3. Recursion & Backtracking/Main4.java
@@ -0,0 +1,39 @@
+
+
+//    https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/combinations-2-official/ojquestion
+
+
+import java.io.*;
+public class Main4 {
+
+  public static void combinations(int[] boxes, int ci, int ti, int lb){
+    
+    if(ci > ti){
+        for(int val : boxes){
+            if(val != 0){
+                System.out.print("i");
+            }else{
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+        return;
+    }
+
+    for(int i = lb + 1; i < boxes.length; i++){
+       if(boxes[i] == 0){
+            boxes[i] = ci;
+            combinations(boxes, ci + 1, ti, i);
+            boxes[i] = 0;
+       }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(new int[nboxes], 1, ritems, -1);
+  }
+
+}
