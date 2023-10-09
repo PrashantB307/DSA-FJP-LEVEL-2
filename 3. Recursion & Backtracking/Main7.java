5f8056afed9c78a918598b40141d5b65faffc16d
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-09 12:03:22 +0530

    Queens Combinations - 2d As 2d - Box Chooses.

---
+++ 3. Recursion & Backtracking/Main7.java
@@ -0,0 +1,32 @@
+
+
+//    https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/queens-combinations-2das2d-box-chooses-official/ojquestion
+
+
+import java.io.*;
+public class Main7 {
+
+    public static void queensCombinations(int qpsf, int tq, int row, int col, String asf){
+        
+        if(row == tq){
+            if(qpsf == tq){
+                System.out.println(asf);
+            }
+            return;
+        }
+
+        if(col == tq - 1){
+            queensCombinations(qpsf + 1, tq, row + 1, 0, asf + "q\n");
+            queensCombinations(qpsf + 0, tq, row + 1, 0, asf + "-\n");
+        }else{
+            queensCombinations(qpsf + 1, tq, row, col + 1, asf + "q");
+            queensCombinations(qpsf + 0, tq, row, col + 1, asf + "-");
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        queensCombinations(0, n, 0, 0, "");
+    }
+}
