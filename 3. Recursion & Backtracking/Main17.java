00d645a1ef62a5da91365268bcb74ec21f8454de
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-13 22:08:53 +0530

    Lexicographical Numbers

---
+++ 3. Recursion & Backtracking/Main17.java
@@ -0,0 +1,32 @@
+
+
+//    https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/lexicographical-numbers-official/ojquestion
+
+import java.util.*;
+public class Main17 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+
+		//write your code here
+        for(int i = 1; i <=9; i++){
+            lexico(i, n);
+        }
+
+        scn.close();
+	}
+
+    public static void lexico(int curr, int n){
+
+        if(curr > n){
+            return;
+        }
+
+        System.out.println(curr);
+
+        for(int i = 0; i <= 9; i++){
+            lexico(curr * 10 + i, n);
+        }
+    }
+	
+}
