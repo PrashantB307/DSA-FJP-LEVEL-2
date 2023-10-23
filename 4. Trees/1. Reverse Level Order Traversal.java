9de3c764f44eb56f02b9a5ca9538799027db3718
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-23 17:24:14 +0530

    Reverse Level Order Traversal

---
+++ 4. Trees/1. Reverse Level Order Traversal.java
@@ -0,0 +1,51 @@
+
+
+//  https://practice.geeksforgeeks.org/problems/reverse-level-order-traversal/1
+
+
+import java.util.*;
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        ArrayList<Integer> ret = new ArrayList<>();
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        
+        Stack<Integer> st = new Stack<>();
+       
+        while(queue.size() > 0){
+            Node temp = queue.remove();
+            
+            st.push(temp.data);
+            
+            if(temp.right != null){
+                queue.add(temp.right);
+            }
+            
+            if(temp.left != null){
+                queue.add(temp.left);
+            }
+            
+        }
+        
+        while(st.size() > 0){
+            int val = st.pop();
+            ret.add(val);
+        }
+        
+        return ret;
+    }
+}
