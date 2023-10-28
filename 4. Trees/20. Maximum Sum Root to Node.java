9726fc1204cc63dd2a4a581fa427b09b4eccd6f6
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-28 11:25:24 +0530

    Maximum Sum Root to Node

---
+++ 4. Trees/20. Maximum Sum Root to Node.java
@@ -0,0 +1,72 @@
+
+
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Solution {
+    public static int maxPathSum(Node root) {
+
+        if (root.left != null && root.right != null) {
+            int leftsum = maxPathSum(root.left);
+            int rightsum = maxPathSum(root.right);
+
+            int left_dash = Math.max(0, leftsum);
+            int right_dash = Math.max(0, rightsum);
+
+            int maxsum = Math.max(left_dash, right_dash) + root.data;
+            return maxsum;
+
+        } else if (root.left != null) {
+            int leftsum = maxPathSum(root.left);
+            int left_dash = Math.max(0, leftsum);
+
+            return left_dash + root.data;
+
+        } else if (root.right != null) {
+            int rightsum = maxPathSum(root.right);
+            int right_dash = Math.max(0, rightsum);
+
+            return right_dash + root.data;
+
+        } else {
+            return root.data;
+        }
+    }
+}
+
+
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+class Solution2 {
+    public static int maxPathSum(Node root) {
+
+        if(root == null){
+            return Integer.MIN_VALUE;
+        }
+        if(root.left == null && root.right == null){
+            return root.data;
+        }
+
+        int leftsum = maxPathSum(root.left);
+        int rightsum = maxPathSum(root.right);
+
+        int left_dash = Math.max(0, leftsum);
+        int right_dash = Math.max(0, rightsum);
+
+        int maxsum = Math.max(left_dash, right_dash) + root.data;
+        return maxsum;
+
+    }
+}
