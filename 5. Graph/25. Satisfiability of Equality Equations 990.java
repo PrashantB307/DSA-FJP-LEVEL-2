268b0bd5b5437a2ec0df5fdd450574521faeed36
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-11-11 14:44:58 +0530

    Satisfiability of Equality Equations 990

---
+++ 5. Graph/25. Satisfiability of Equality Equations 990.java
@@ -0,0 +1,71 @@
+
+
+//    https://leetcode.com/problems/satisfiability-of-equality-equations/
+
+
+class Solution {
+
+    int [] parent;
+    int [] rank;
+
+    public boolean equationsPossible(String[] equations) {
+        
+        parent = new int [26];
+        rank = new int [26];
+
+        for(int i = 0; i < 26; i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+
+        for(String equation : equations){
+            if(equation.charAt(1) == '='){
+                int op1 = equation.charAt(0) - 'a';
+                int op2 = equation.charAt(3) - 'a';
+
+                int l1 = find(op1);
+                int l2 = find(op2);
+
+                if(l1 != l2){
+                    union(l1, l2);
+                }
+            }
+        }
+
+        for(String equation : equations){
+            if(equation.charAt(1) == '='){
+                int op1 = equation.charAt(0) - 'a';
+                int op2 = equation.charAt(3) - 'a';
+
+                int l1 = find(op1);
+                int l2 = find(op2);
+
+                if(l1 == l2){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public int find(int x){
+        if(parent[x] == x){
+            return parent[x];
+        }else{
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+    }
+
+    public void union(int x, int y){
+        if(rank[x] < rank[y]){
+            parent[x] = y;
+        }else if(rank[y] < rank[x]){
+            parent[y] = x;
+        }else{
+            parent[x] = y;
+            rank[y]++;
+        }
+    }
+} 
