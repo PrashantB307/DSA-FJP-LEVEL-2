3cb66f57cfda41c9a7c8768f5c5f26c11b0c6011
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-11-24 17:16:22 +0530

    Minimum Platforms

---
+++ 6. Arrays & Strings/19. Minimum Platforms.java
@@ -0,0 +1,34 @@
+
+
+
+//     https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1
+
+
+import java.util.*;
+class Solution
+{
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+
+        int count = 0;
+        int max = 0;
+
+        int i = 0, j = 0;
+        while(i < arr.length && j < dep.length){
+            if(arr[i] <= dep[j]){
+                i++;
+                count++;
+            }else{
+                j++;
+                count--;
+            }
+
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+}
+
