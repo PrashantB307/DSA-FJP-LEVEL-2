f6bdf04553da13228ddd6cfdf6876ae61d3035d4
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-11-28 20:12:25 +0530

    Overlapping Intervals Merge Intervals 56

---
+++ 6. Arrays & Strings/37. Overlapping Intervals Merge Intervals 56.java
@@ -0,0 +1,33 @@
+
+
+//     https://leetcode.com/problems/merge-intervals/
+
+//     https://practice.geeksforgeeks.org/problems/overlapping-intervals--170633/1
+
+
+import java.util.*;
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        
+        Arrays.sort(intervals, (a, b) -> {
+           return a[0] - b[0];
+        });
+
+        ArrayList<int[]> list = new ArrayList<>();
+
+        for(int[] interval : intervals){
+            if(list.size() == 0){
+                list.add(interval);
+            }else{
+                int[] last = list.get(list.size() - 1);
+                if(interval[0] > last[1]){
+                    list.add(interval);
+                }else{
+                    last[1] = Math.max(last[1], interval[1]);
+                }
+            }
+        }
+
+        return list.toArray(new int[list.size()][2]);
+    }
+} 
