4faaab1b3b858f6bf730dd2923ceef8a3b3ba726
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-12-16 23:53:15 +0530

    Minimize the Heights 2

---
+++ 6. Arrays & Strings/Minimize the Heights 2.java
@@ -0,0 +1,32 @@
+
+
+//   https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1
+
+//   https://www.codingninjas.com/studio/problems/minimize-the-difference_3208652
+
+
+import java.util.*;
+class Solution {
+    int getMinDiff(int[] arr, int n, int k) {
+        
+        Arrays.sort(arr);
+        int largest = arr[n - 1] - k;
+        int smallest = arr[0] + k;
+        int diff = arr[n - 1] - arr[0];
+       
+        int min, max;
+        
+        for(int i = 0; i < n - 1; i++){
+            min = Math.min(smallest, arr[i + 1] - k);
+            max = Math.max(largest, arr[i] + k);
+           
+            if(min < 0){
+               continue;
+            }
+            
+            diff  = Math.min(diff, max - min);
+        }
+        
+        return diff;
+    }
+}
