80611169a7acc278865295953da3557ef2f93be9
Prashant Kumar <pb28.rkt@gmail.com>
2023-12-29 17:54:26 +0530

    Number of Digit One 233

---
+++ 7. Dynamic Programming/46. Number of Digit One 233.java
@@ -0,0 +1,33 @@
+
+
+//   https://leetcode.com/problems/number-of-digit-one/description/
+ 
+//   https://www.codingninjas.com/studio/problems/count-number-of-ones_1461958
+
+
+class Solution {
+    public int countDigitOne(int n) {
+        
+        int res = 0;
+        int place = 1;
+
+        while (place <= n) {
+            int q = n / (10 * place);
+            int r = n % (10 * place);
+
+            int extra = 0;
+            if(r >= 2 * place){
+                extra = place;
+            }else if(r >= place){
+                extra = r - place + 1;
+            }else{
+                extra = 0;
+            }
+
+            res += q * place + extra;
+            place = place * 10;
+        }
+
+        return res;
+    }
+}
