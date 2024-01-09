183a2f3db851f3e7d4a7a1bd753860ce51cb6a85
Prashant Kumar <pb28.rkt@gmail.com>
2024-01-09 18:55:34 +0530

    Remove K Digits 402

---
+++ 8. Stack & Queue/14. Remove K Digits 402.java
@@ -0,0 +1,53 @@
+
+
+//    https://leetcode.com/problems/remove-k-digits/
+
+//    https://practice.geeksforgeeks.org/problems/remove-k-digits/1
+
+//    https://www.codingninjas.com/studio/problems/remove-k-digits_1461221
+
+
+import java.util.*;
+class Solution {
+    public String removeKdigits(String num, int k) {
+        
+        if(k == num.length()){
+            return "0";
+        }
+
+        Stack<Character> st = new Stack<>();
+        st.push(num.charAt(0));
+
+        for(int i = 1; i < num.length(); i++){
+            char ch = num.charAt(i);
+
+            while(st.size() > 0 && k > 0 && ch < st.peek()){
+                st.pop();
+                k--;
+            }
+
+            st.push(ch);
+        }
+
+        while(k > 0){
+            st.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(st.size() > 0){
+            sb.append(st.pop());
+        }
+
+        for(int i = sb.length() - 1; i >= 0; i--){
+            if(sb.charAt(i) == '0'){
+                sb.deleteCharAt(i);
+            }else{
+                break;
+            }
+        }
+
+        String res = sb.reverse().toString();
+        return res.length() == 0 ? "0" : res;
+    }
+}
